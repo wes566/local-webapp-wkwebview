83954d12fcbc19d3692a1d8bc3ceabf7cf49bb58
Wes Peter <wes@wespeter.com>
2018-11-08 15:51:57 -0800

    add simple webapp that shows image

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # local-webapp-wkwebview
+
 A sample loading a webapp in a wkwebview on iOS
+
+## To copy webapp to ios
+
+```bash
+# from root of repo
+rm -rf ios/wvapp/webapp && cp -r ./webapp/. ios/wvapp/webapp
+```
---
+++ ios/wvapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wvapp
+//
+//  Created by Wes Peter on 11/8/18.
+//  Copyright © 2018 Wes Peter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
