9dab0f6f2e244cd1a18922610e5f2ff4a2491449
Wes Peter <wes@wespeter.com>
2019-01-14 08:59:12 -0800

    make text bigger so I can read it

--- ios/wvapp/ViewController.swift
+++ ios/wvapp/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         webView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(webView)
         
